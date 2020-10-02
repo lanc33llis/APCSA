f4bb1f1262c37cadcebbdad9f9485e50b8674dd9
Lance Ellis <scdelance@gmail.com>
2020-10-01 19:07:19 -0500

    now prints source code

--- main.java
+++ main.java
@@ -1,16 +1,12 @@
 import java.util.*;
-// import edhesive.shapes.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
 
-class a{
-  public int i;
-  a(int b){
-    i = b;
-  }
-}
-
-class U2_L1_Activity_Two{
+class rem{
     public static Scanner scan = new Scanner(System.in);
-    
+
+    private static String name = "test";
+
     public static void print(java.lang.Object obj){
       System.out.println(obj);
     }
@@ -21,17 +17,88 @@ class U2_L1_Activity_Two{
       }
     }
 
-    public static void printarray(Vector<java.lang.Object> a)
-    {
-      for (int i = 0; i < a.size(); i++){
-        print(a.get(i));
-      }
+    public static void printarray(Vector<java.lang.Object> a) {
+        for (int i = 0; i < a.size(); i++) {
+            print(a.get(i));
+        }
     }
 
-    public static void main(String[] args){
-      Integer a = new Integer(12);
-      Integer b = new Integer(22);
-      print(a.compareTo(b));
+    static{
+        rem a = new rem();
+        a = null;
+        System.out.print("Name of Class: ");
+        name = scan.nextLine();
+        System.gc();
+    }
 
+    public static void main(String[] args) throws InterruptedException {
+        /*REMOVE*/TimeUnit.MILLISECONDS.sleep(25);
+
+        //MAIN CODE STARTS
+
+        print("What type of item are you buying?");
+        String a = scan.nextLine();
+        print("How many are you buying?");
+        int b = scan.nextInt();
+        print("How much does each one cost?");
+        double c = scan.nextDouble();
+        double sum = b * c;
+        print(b + " " + a + " at $" + c + " each will cost you $" +  sum);
+
+        //MAIN CODE ENDS
+
+        /*REMOVE*/try {
+            TimeUnit.MILLISECONDS.sleep(25);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void finalize(){
+        try{
+            FileInputStream stream = new FileInputStream("main.java");
+            DataInputStream in = new DataInputStream(stream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            String s;
+
+            print("");
+            print("------SOURCE CODE------");
+            print("");
+
+            while((s = br.readLine() ) != null)
+            {
+                if (s.contains("class "+"rem{")){
+                    print("class " + name + "{");
+                }
+                else if (s.contains("static{")) {
+                    br.readLine();
+                    br.readLine();
+                    br.readLine();
+                    br.readLine();
+                    br.readLine();
+                    br.readLine();
+                }
+                else if (s.contains("/*REMOVE*/try")){
+                    print("    }");
+                    print("}");
+                    in.close();
+                    break;
+                }
+                else if (s.contains("/*REMOVE*/Time")){
+                    br.readLine();
+                }
+                else{
+                   print(s);
+                }
+            }
+
+            print("");
+            print("------NORMAL RUN------");
+            print("");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
